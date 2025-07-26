f3f9296f0e70d2dc96942f599de75249999ce1f9
新しい僕 <new_me@example.com>
2025-07-26 23:17:09 +0900

    daily work

--- src/main/java/jp/dsm/explore/item/TestItem.java
+++ src/main/java/jp/dsm/explore/item/TestItem.java
@@ -1,4 +1,12 @@
 package jp.dsm.explore.item;
 
-public class TestItem {
+import net.minecraft.world.food.FoodProperties;
+
+public class TestItem extends BaseItem{
+
+    public static final Properties PROP = new Properties().food(new FoodProperties(1, 1, true));
+
+    public TestItem(String id) {
+        super(PROP, id);
+    }
 }
