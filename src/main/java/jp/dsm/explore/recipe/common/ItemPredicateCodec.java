de921cc46de9e233c877295790edb8c34e3deb43
DaiDevJpn <dai.dev.jpn123@gmail.com>
2025-08-19 11:03:00 +0900

    daily work

--- src/main/java/jp/dsm/explore/recipe/common/ItemPredicateCodec.java
+++ src/main/java/jp/dsm/explore/recipe/common/ItemPredicateCodec.java
@@ -1,6 +1,5 @@
 package jp.dsm.explore.recipe.common;
 
-import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
